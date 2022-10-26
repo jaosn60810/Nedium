e00317a22ae889c36c75705a735a303e9444577b
jason60810 <jason60810@gmail.com>
2022-10-26 13:46:58 +0800

    build: clean up

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt></Nuxt>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>hihi</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
